050074f9561c3118fb929600d3e2123d9b141326
hou <343382405@qq.com>
2021-01-16 23:29:43 +0800

    前端更新

--- vue/frontpage/src/components/Demo.vue
+++ vue/frontpage/src/components/Demo.vue
@@ -7,6 +7,51 @@
             <el-col :span="12"><div class="grid-content bg-purple"></div></el-col>
             <el-col :span="12"><div class="grid-content bg-purple-light"></div></el-col>
         </el-row>
+        <el-row>
+            <el-col :span="8"><div class="grid-content bg-purple"></div></el-col>
+            <el-col :span="8"><div class="grid-content bg-purple-light"></div></el-col>
+            <el-col :span="8"><div class="grid-content bg-purple"></div></el-col>
+        </el-row>
+        <el-row :gutter="20">
+            <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+            <el-col :span="6"><div class="grid-content bg-purple-light"></div></el-col>
+            <el-col :span="6"><div class="grid-content bg-purple"></div></el-col> 
+            <el-col :span="6"><div class="grid-content bg-purple-light"></div></el-col>
+        </el-row>
+        <el-row>
+            <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
+            <el-col :span="4"><div class="grid-content bg-purple-light"></div></el-col>
+            <el-col :span="4"><div class="grid-content bg-purple"></div></el-col> 
+            <el-col :span="4"><div class="grid-content bg-purple-light"></div></el-col>
+            <el-col :span="4"><div class="grid-content bg-purple"></div></el-col> 
+            <el-col :span="4"><div class="grid-content bg-purple-light"></div></el-col>
+        </el-row>
+        <el-row :gutter="20">
+            <el-col :span="16"><div class="grid-content bg-purple"></div></el-col>
+            <el-col :span="8"><div class="grid-content bg-purple"></div></el-col>
+        </el-row>
+        <el-row :gutter="20">
+            <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+            <el-col :span="6" :offset="6"><div class="grid-content bg-purple"></div></el-col>
+        </el-row>
+        <el-row type="flex" class="row-bg" justify="center">
+            <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+            <el-col :span="6"><div class="grid-content bg-purple-light"></div></el-col>
+            <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+        </el-row>
+        <el-row :gutter="10">
+            <el-col :xs="8" :sm="6" :md="4" :lg="3" :xl="1"><div class="grid-content bg-purple"></div></el-col>
+            <el-col :xs="4" :sm="6" :md="8" :lg="9" :xl="11"><div class="grid-content bg-purple"></div></el-col>
+        </el-row>
+        <el-container>
+            <el-header>Header</el-header>
+            <el-main>Main</el-main>
+            <el-footer>Footer</el-footer>
+        </el-container>
+        <el-container>
+            <el-aside width="200px">Aside</el-aside>
+            <el-main>Main</el-main>
+        </el-container>
     </div>
 </template>
 
@@ -42,5 +87,33 @@ export default {
     .row-bg{
         padding: 10px 0;
         background-color: #f9fafc;
+    }
+    .el-header, .el-footer{
+        background-color: #B2C0D1;
+        color: #333;
+        text-align: center;
+        line-height: 60px;
+    }
+    .el-aside{
+        background-color: #D3DCE6;
+        color: #333;
+        text-align: center;
+        line-height: 200px;
+    }
+    .el-main{
+        background-color: #E9EEF3;
+        color: #333;
+        text-align: center;
+        line-height: 160px;
+    }
+    body > .el-container{
+        margin-bottom: 40px;
+    }
+    .el-container:nth-child(5) .el-aside,
+    .el-container:nth-child(6) .el-aside{
+        line-height: 260px;
+    }
+    .el-container:nth-child(7) .el-aside{
+        line-height: 320px;
     }
 </style>
---
+++ vue/frontpage/src/components/Xuancard.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <el-container style="height: 500px; border: 1px solid #eee">
+            <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
+                <el-menu :default-openeds="['1', '3']">
+                    <el-submenu index="1">
+                        <template slot="title"><i class="el-icon-message"></i>导航一</template>
+                        <el-menu-item-group>
+                            <template slot="title">分组一</template>
+                            <el-menu-item index="1-1">选项1</el-menu-item>
+                            <el-menu-item index="1-2">选项2</el-menu-item>
+                        </el-menu-item-group>
+                    </el-submenu>
+                </el-menu>
+            </el-aside>
+        </el-container>
+    </div>
+    
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+
+</style>
